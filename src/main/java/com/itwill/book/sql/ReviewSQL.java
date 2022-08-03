ce6581abc630fe050766c2bdfb85dc79586fe11c
imeunbi <amy980524@naver.com>
2022-08-03 17:34:15 +0900

    ..

--- src/main/java/com/itwill/book/sql/ReviewSQL.java
+++ src/main/java/com/itwill/book/sql/ReviewSQL.java
@@ -1,33 +1,9 @@
 package com.itwill.book.sql;
 
 public class ReviewSQL {
-	public static final String REVIEW_INSERT = 
-			"insert into review (r_no, r_title,r_contents, r_date,r_grade,u_id,od_no) values(REVIEW_R_NO_SEQ.nextval,?,?,sysdate,?,?,?)";
-	public static final String REVIEW_UPDATE = 
-			"update review set r_title=?, r_contents=?, r_grade=? where r_no=?";
+	public static final String INSERT_REVIEW_NEW = "insert into review (r_no,r_title,r_contents,r_date,r_grade,u_id,od_no,r_groupno,r_step,r_depth) values(REVIEW_R_NO_SEQ.nextval, ?,?,sysdate,?,?,?, REVIEW_R_NO_SEQ.nextval,?,?)";
 	
-	public static final String REVIEW_DELETE = "delete review where r_no=?";
+	public static final String INSERT_REVIEW_REPLY = "";
 	
 	
-	//리뷰 번호로 리뷰 조회
-	public static final String REVIEW_SELECT_BY_R_NO = "select * from review where r_no=?";
-	
-/*	
-	//책 번호로 리뷰 조회
-	public static final String REVIEW_SELECT_BY_B_NO = "select * from review where b_no=?";
-*/
-	
-	//책 번호로 리뷰 조회
-	public static final String REVIEW_SELECT_BY_B_NO = "select * from review join orderdetail on r.od_no= od.od_no where od.b_no=?";
-	
-	
-	
-/*	
-	//회원아이디로 리뷰 조회(마이페이지에서 리뷰조회)
-	public static final String REVIEW_SELECT_BY_U_ID = 
-			"select * from (select * from review r join orders o on r.o_no=o.o_no) join userinfo on u.u_id=ro.u_id where u.u_id=?";
-*/	
-	//회원아이디로 리뷰조회-아이디만 이용
-	public static final String REVIEW_SELECT_BY_U_ID = 
-			"select * from review where u_id=?";
 }
