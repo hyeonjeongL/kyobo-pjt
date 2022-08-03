9dccd67192b43686bda2c8633b2ac9c923b38a96
예쁘고 당당한 지훈공주 <  @DESKTOP-FI0V939>
2022-08-03 17:24:18 +0900

    ..

--- src/main/java/com/itwill/book/dao/test/OrderDaoTest.java
+++ src/main/java/com/itwill/book/dao/test/OrderDaoTest.java
@@ -8,22 +8,20 @@ import com.itwill.book.dao.CartDao;
 import com.itwill.book.dao.OrderDao;
 import com.itwill.book.dto.Book;
 import com.itwill.book.dto.OrderDetail;
+import com.itwill.book.dto.Orders;
 
 public class OrderDaoTest {
 
 	public static void main(String[] args) throws Exception{
-		OrderDao orderDao = new OrderDao();
-		CartDao cartDao = new CartDao();
-		BookDao bookDao = new BookDao();
+		 OrderDao orderDao = null;
+		 CartDao cartDao = null;
+		 BookDao bookDao = null;
+		//주문을 넣어보자 상품 페이지에서 직접 주문
+		// book 번호 1
+		 String b_name = "마시멜로이야기";
 		
-		int b_no = 1;
-		int b_qty = 1;
-		
-		List<OrderDetail> OrderDetailList = new ArrayList<OrderDetail>();
-		//Book book = bookDao.selectByName("마시멜로이야기");
-		ArrayList<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
-	//미완성
-	
+		 List<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
+		 Book book = bookDao.selectByName(b_name);
 	}
 
 }
