d84527c0917ae8392254e2b343e5a0785bacf6b3
hunnnnii94 <user@192.168.10.100>
2022-08-04 16:11:06 +0900

    FOR 봉균

--- src/main/java/com/itwill/book/dao/CartDao.java
+++ src/main/java/com/itwill/book/dao/CartDao.java
@@ -74,7 +74,7 @@ public class CartDao {
 	//cart deleteByNo 카트 선택삭제
 	public int cartDeleteByNo (int b_no) throws Exception{
 		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(CartSQL.CART_DELETE_BY_NO);
+		PreparedStatement pstmt = con.prepareStatement(CartSQL.CART_DELETE_BY_CNO);
 		pstmt.setInt(1, b_no);
 		
 		int deleteNoCount = pstmt.executeUpdate();
@@ -115,6 +115,33 @@ public class CartDao {
 		}
 		con.close();
 		return cartList;
+	}
+	
+	//카트리스트(카트넘버) 주문용
+	public List<Cart> cartListOder(String c_no) throws Exception{
+		
+		List<Cart> cartListOrder = new ArrayList<Cart>();
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(CartSQL.CART_SELECT_BY_CNO);
+		pstmt.setString(1, c_no);
+		ResultSet rs = pstmt.executeQuery();
+		while(rs.next()) {
+			Cart cart = new Cart(rs.getInt("c_no"),
+								 rs.getInt("c_qty"),
+								 rs.getString("u_id"),
+								 new Book(rs.getInt("b_no"),
+										 rs.getString("b_class"),
+										 rs.getString("b_name"),
+										  rs.getInt("b_price"),
+										  rs.getString("b_summary"),
+										  rs.getString("b_image"),
+										  rs.getString("b_author"),
+										  rs.getString("b_publisher")));
+							cartListOrder.add(cart);
+		}
+		con.close();
+		return cartListOrder;
+		
 	}
 	
 	
