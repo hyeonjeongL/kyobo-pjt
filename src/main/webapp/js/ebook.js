46920bc6e2440a06c27bdbf3b367d4be21b3af68
seongmin <posteto@naver.com>
2022-08-05 15:51:12 +0900

    ..

--- src/main/webapp/js/ebook.js
+++ src/main/webapp/js/ebook.js
@@ -7,3 +7,10 @@ function ebookBuy() {
 		document.add_cart_form.submit();
 	}
 	
+	
+function setCookies(){
+		document.ebook.action = "ebook_registered.jsp";
+		document.ebook.method = "POST";
+		document.ebook.submit();
+	
+}
