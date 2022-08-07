300fb9bc91f963e9a0b4bfecdd828d04d589bf74
성민 <posteto@naver.com>
2022-08-07 17:39:18 +0900

    book.js

--- src/main/webapp/js/book.js
+++ src/main/webapp/js/book.js
@@ -1,3 +1,10 @@
 /**
  * 
  */
+
+function changeqty() {
+	var cart_qty = document.getElementById('cart_qty').value;
+	
+	document.getElementById('b_qty').value = cart_qty;
+	console.log(cart_qty);
+}
